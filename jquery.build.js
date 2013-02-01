0f11a5f9758d1f62e6868b367cfa5dff82a01951
Baptiste Pillot <baptiste@pillot.fr>
2013-02-01 22:41:02 +0100

    initial version

---
+++ jquery.build.js
@@ -0,0 +1,35 @@
+(function($)
+{
+
+	// this (global) static variable stores all wished build() callbacks
+	window.jquery_build_callback = [];
+
+	/**
+	 * Call build(callback) what callback functions you want to be called for future added dom elements
+	 * call this.build() after you add dom elements (ie dynamic javascript add, ajax calls) to apply the same changes
+	 *
+	 * @param callback function the callback function
+	 * @param call_now boolean optional default true
+	 */
+	$.fn.build = function (callback, call_now)
+	{
+		call_now = call_now || true;
+		if (callback != undefined) {
+			window.jquery_build_callback.push(callback);
+			if ((call_now == undefined) || call_now) {
+				this.tmpBuildCaller = callback;
+				this.tmpBuildCaller();
+				delete this.tmpBuildCaller;
+			}
+		}
+		else {
+			for (var key in jquery_build_callback) {
+				callback = window.jquery_build_callback[key];
+				this.tmpBuildCaller = callback;
+				this.tmpBuildCaller();
+			}
+			delete this.tmpBuildCaller;
+		}
+	}
+
+})( jQuery );
